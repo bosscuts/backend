ebfac1e2d204fc049794e0e3db748df822fd927d
Quang Hoa <hoaronal-project>
2022-06-03 00:28:08 +0700

    init commit

---
+++ src/main/java/com/bosscuts/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.bosscuts.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
