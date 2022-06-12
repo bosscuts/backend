b92e46a5f200c83e5848d911e37cd5e7bdae01f9
Quang Hoa <hoaronal-project>
2022-06-13 00:57:56 +0700

    sd

--- src/main/java/com/bosscut/config/SecurityConfiguration.java
+++ src/main/java/com/bosscut/config/SecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.bosscut.config;
 
 import com.bosscut.repository.UserRepository;
 import com.bosscut.service.DomainUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,7 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserRepository userRepository;
 
     public SecurityConfiguration(UserRepository userRepository) {
@@ -32,8 +36,8 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
                 .antMatchers("/h2-console/**", "/users/**", "/login", "/register").permitAll()
@@ -42,22 +46,37 @@ public class SecurityConfiguration {
 //                .hasAnyAuthority("Admin", "Editor", "Salesperson")
 //                .hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
                 .anyRequest().authenticated()
-                .and().formLogin()
+                .and()
+                .formLogin()
                 .loginPage("/login")
-                .usernameParameter("email")
                 .permitAll()
                 .and()
                 .rememberMe().key("AbcdEfghIjklmNopQrsTuvXyz_0123456789")
                 .and()
-                .logout().permitAll();
+                .logout()
+                .logoutSuccessUrl("/login?logout").permitAll();
+        http.csrf().disable();
+//        http.headers().frameOptions().disable();
+//        http.headers().frameOptions().sameOrigin();
 
-        http.headers().frameOptions().sameOrigin();
-
-        return http.build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/h2-console/**", "/img/**", "/js/**", "/css/**", "/webjars/**");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceBean())
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("password")).roles("ADMIN");
     }
 }
--- src/main/java/com/bosscut/controller/LoginController.java
+++ src/main/java/com/bosscut/controller/LoginController.java
@@ -1,15 +1,28 @@
 package com.bosscut.controller;
 
+import com.bosscut.dto.LoginDTO;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
     public String showLoginPage() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "login";
+        }
+        return "redirect:/users";
+    }
+
+    @PostMapping("/login1111")
+    public String login(@RequestBody LoginDTO loginDTO) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
