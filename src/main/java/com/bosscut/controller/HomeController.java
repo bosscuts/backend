863da0dcc6240e473e9fd8b06b808ccfc5fc42e6
Hoa Doan <hoadoan@quanghoa.local>
2022-07-24 01:02:52 +0700

    crawl dien may xanh

--- src/main/java/com/bosscut/controller/HomeController.java
+++ src/main/java/com/bosscut/controller/HomeController.java
@@ -2,15 +2,11 @@ package com.bosscut.controller;
 
 import com.bosscut.entity.CrawlUrl;
 import com.bosscut.repository.CrawlRepository;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
